3c0a3366beb4350d44f8b2f8556ae12d24e3f444
sunday stanley Idung <sundaystanley56@gmail.com>
2022-06-13 21:26:46 +0100

    Add link and route

--- src/Containers/SpaceTravelersHub/SpaceTravelersHub.jsx
+++ src/Containers/SpaceTravelersHub/SpaceTravelersHub.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import MyProfile from '../MyProfile/MyProfile';
+import Rockets from '../Rockets/Rockets';
 
 const SpaceTravelersHub = () => (
   <>
@@ -10,6 +11,7 @@ const SpaceTravelersHub = () => (
       <Routes>
         <Route path="/" element={<div>Main page</div>} />
         <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/rockets" element={<Rockets/>}/>
       </Routes>
     </BrowserRouter>
   </>
