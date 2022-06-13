0639a9ac9db9bce1aea1142035f8069783d8730a
Andres Agudelo <anagudelogu@gmail.com>
2022-06-13 17:10:26 -0500

    Set the provider and load data on render

--- src/App/SpaceTravelersHub.jsx
+++ src/App/SpaceTravelersHub.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from '../Containers/Navbar/Navbar';
 import MyProfile from '../Containers/MyProfile/MyProfile';
 import Rockets from '../Containers/Rockets/Rockets';
+import { readMissionsData } from '../redux/mission';
 
-const SpaceTravelersHub = () => (
-  <>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<div>Main page</div>} />
-        <Route path="/my-profile" element={<MyProfile />} />
-        <Route path="/rockets" element={<Rockets />} />
-      </Routes>
-    </BrowserRouter>
-  </>
-);
+const SpaceTravelersHub = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(readMissionsData());
+  });
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<div>Main page</div>} />
+          <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/rockets" element={<Rockets />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default SpaceTravelersHub;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './App/configureStore';
 import SpaceTravelersHub from './App/SpaceTravelersHub';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SpaceTravelersHub />
+    <Provider store={store}>
+      <SpaceTravelersHub />
+    </Provider>
   </React.StrictMode>,
 );
