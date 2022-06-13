da027a0fae3132ee81536a8b9b1a631be0febdd5
Andres Agudelo <anagudelogu@gmail.com>
2022-06-13 16:24:11 -0500

    Fix import on index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SpaceTravelersHub from './Containers/SpaceTravelersHub/SpaceTravelersHub';
+import SpaceTravelersHub from './App/SpaceTravelersHub';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
