52d9ff4721b9da34e9851b6cc865b8587de2d76b
sunday stanley Idung <sundaystanley56@gmail.com>
2022-06-13 23:14:49 +0100

    Fix linters error

--- src/redux/rockets.js
+++ src/redux/rockets.js
@@ -1,21 +1,21 @@
-import { getRocketsFromAPI } from '../services/spaceXAPI'
-//Action
-const READ = 'space_hub_traveler/reducer/READ'
+import { getRocketsFromAPI } from '../services/spaceXAPI';
+// Action
+const READ = 'space_hub_traveler/reducer/READ';
 
-//Reducer
-const rocketReducer= (state, action) => {
+// Reducer
+const rocketReducer = (state, action) => {
   switch (action.type) {
     case READ:
       return action.payload;
-      default:
-       return state;
+    default:
+      return state;
   }
-}
+};
 
-//Action creator
-export const readRocketData =()=> async (dispatch)=> {
- const res =await getRocketsFromAPI();
- dispatch({type: READ, payload: res})
-}
+// Action creator
+export const readRocketData = () => async (dispatch) => {
+  const res = await getRocketsFromAPI();
+  dispatch({ type: READ, payload: res });
+};
 
-export default rocketReducer
+export default rocketReducer;
