69615d1a9780340716c1295237380bf72e8dbae5
Andres Agudelo <anagudelogu@gmail.com>
2022-06-13 16:17:46 -0500

    Re-structure folder organization, created a new folder APP

--- src/App/SpaceTravelersHub.jsx
+++ src/App/SpaceTravelersHub.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from '../Navbar/Navbar';
-import MyProfile from '../MyProfile/MyProfile';
-import Rockets from '../Rockets/Rockets';
+import Navbar from '../Containers/Navbar/Navbar';
+import MyProfile from '../Containers/MyProfile/MyProfile';
+import Rockets from '../Containers/Rockets/Rockets';
 
 const SpaceTravelersHub = () => (
   <>
