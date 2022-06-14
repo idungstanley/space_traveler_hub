a4a7cd3476ae7de1935fe959e42133c3de1a73a0
Andres Agudelo <anagudelogu@gmail.com>
2022-06-14 14:58:29 -0500

    Add handleJoin and handleLeave functions to useMission hook

--- src/hooks/useMission.js
+++ src/hooks/useMission.js
@@ -1,10 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { joinMission, leaveMission } from '../redux/mission';
 
 const useMission = () => {
+  const dispatch = useDispatch();
   const missions = useSelector((state) => state.missions);
+
+  const handleJoinMission = (id) => {
+    dispatch(joinMission(id));
+  };
+
+  const handleLeaveMission = (id) => {
+    dispatch(leaveMission(id));
+  };
 
   return {
     missions,
+    handleJoinMission,
+    handleLeaveMission,
   };
 };
 
