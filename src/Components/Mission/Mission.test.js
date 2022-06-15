46b332ce7b6fe90e8d793ea3fc03bf576f52c354
Andres Agudelo <anagudelogu@gmail.com>
2022-06-15 17:08:38 -0500

    Install Hamburger icon and Fix test with recent prop changes

--- src/Components/Mission/Mission.test.js
+++ src/Components/Mission/Mission.test.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
 import Mission from './Mission';
+import store from '../../App/configureStore';
 
 describe('Mission component', () => {
   it('Renders', () => {
     render(
-      <Mission
-        status="TestStatus"
-        description="TestDesc"
-        missionName="TestName"
-      />,
+      <Provider store={store}>
+        <Mission
+          reserved
+          description="TestDesc"
+          missionName="TestName"
+          missionId="TestId"
+        />
+        ,
+      </Provider>,
     );
     expect(screen.getByRole('button')).toBeInTheDocument();
     expect(screen.getByText('TestName')).toBeInTheDocument();
     expect(screen.getByText('TestDesc')).toBeInTheDocument();
-    expect(screen.getByText('TestStatus')).toBeInTheDocument();
+    expect(screen.getByText('Active Member')).toBeInTheDocument();
   });
 });
