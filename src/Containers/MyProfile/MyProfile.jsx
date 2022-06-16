ff30d16a0f04ee88cda0eb8eed4bda2a9618194e
sunday stanley Idung <sundaystanley56@gmail.com>
2022-06-16 11:16:58 +0100

    Add the book/active rockets to display in the profile tab

--- src/Containers/MyProfile/MyProfile.jsx
+++ src/Containers/MyProfile/MyProfile.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import useMission from '../../hooks/useMission';
 
 const MyProfile = () => {
   const { activeMissions } = useMission();
+  const Data = useSelector((state) => state.rockets);
+  const activeRockets = Data.filter((rocket) => rocket.reserved);
+  console.log(activeRockets);
   return (
     <section>
       <div>
@@ -10,6 +14,14 @@ const MyProfile = () => {
         <ul>
           {activeMissions.map(({ missionName, missionId }) => (
             <li key={missionId}>{missionName}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>My Rockets</h2>
+        <ul>
+          {activeRockets.map(({ rocketId, title }) => (
+            <li key={rocketId}>{title}</li>
           ))}
         </ul>
       </div>
